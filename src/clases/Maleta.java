21ca47502939f21c8be0ab7e5172c7c1dade963f
Javier.R.C <culebras.jr@gmail.com>
2024-05-12 11:15:56 +0200

    2024-05-12--inicio del proyecto

---
+++ src/clases/Maleta.java
@@ -0,0 +1,57 @@
+package clases;
+
+//import java.util.Arrays;
+
+public class Maleta <T>{
+	protected T[]elementos;
+	protected int num;
+	
+	public Maleta (T[]elementos, int num) {
+		this.elementos=elementos;
+		this.num=num;
+	}
+
+	
+	//getters & setters
+	public T[] getElementos() {
+		return elementos;
+	}
+
+	public void setElementos(T[] elementos) {
+		this.elementos = elementos;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	@Override
+	public String toString() {
+		String info="\n\t- Numero de elementos: "+num;
+		for(int i=0; i<num; i++) {
+			info+="\n\t"+i+"- "+elementos[i];
+		}
+		
+		return info;
+	}
+	
+	public void addElem(T  elemento) {
+		
+		elementos[num]= elemento;
+		num++;
+	}
+	
+	public void deleteElem(int ele) {
+		for(int i=ele; i<elementos.length-1; i++) {  
+			//lenght-1 para que no sobrepase el tamaño del array dentro del bucle
+			
+			elementos[i]=elementos[i+1];
+			
+		}
+		num--;
+	}
+}
---
+++ src/principal/Main_maleta.java
@@ -0,0 +1,60 @@
+package principal;
+
+import java.util.Scanner;
+
+import clases.Maleta;
+
+
+public class Main_maleta {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		String[]elementos=new String[100];
+		
+		System.out.println("Cuantos elementos quieres meter en la maleta");
+		int num=sc.nextInt();
+		
+		for(int i=0; i<num; i++) {
+			System.out.println(i+"- ¿Que elemento desea guardar?");
+			String ele=sc.next();
+			elementos[i]=ele;
+		}
+		
+		Maleta <String> maleta=new Maleta <>(elementos,num);
+		
+		int n=0;
+		do {
+			System.out.println("\n- Maleta de viaje");
+			System.out.println("\t1. Ver mi maleta");
+			System.out.println("\t2. Añadir un nuevo elemento a mi maleta");
+			System.out.println("\t3. Borrar un elemento de mi maleta");
+			System.out.println("\t4. Salir");
+			System.out.print("- Elija una opcion: ");
+			n=sc.nextInt();
+			
+			if(n==1) {
+				System.out.println(maleta.toString());
+			}else if(n==2) {
+				System.out.print("- Introduzca el elemento que desea guardar: ");
+				String nEle=sc.next(); 
+				
+				maleta.addElem(nEle);
+				
+			}else if(n==3) {
+				System.out.println(maleta.toString());
+				
+				System.out.println("- Introduzca el numero del elemento que desea eliminar: ");
+				int nEle=sc.nextInt();
+				
+				maleta.deleteElem(nEle);
+				
+			}
+			
+		}while(n!=4);
+
+		sc.close();
+
+	}
+
+}
